344c1f4d4495078b421d1fbd45b5c41929d0722f
Elisabeth Mansfield <elisjmansfield@gmail.com>
2018-09-09 19:37:05 -0400

    Add files via upload

---
+++ harmony.js
@@ -0,0 +1,3 @@
+alert('This website may cause bankrupcy we my be small. But just wait for it!!!!!!!!! This is slowly eating away at your credit card');
+var name=prompt("Enter your Username ");
+alert(" Hola Bounjour Hello Ciao Aloha "+ name+"!!!!! ;) :0 :)");
---
+++ maddie.js
@@ -0,0 +1,4 @@
+// console.log('hi');
+
+var name = prompt("Welcome! What is your name?");
+alert("Hello " + name + "!");
